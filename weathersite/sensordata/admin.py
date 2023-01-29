a85b76d93aa260c5f806d20b662c5f77d0b43146
Pau Amargant <90143513+pauamargant@users.noreply.github.com>
2023-01-29 13:31:15 +0100

    initial commit

---
+++ weathersite/sensordata/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Measure, Sensor, Measurement
+
+
+
+# Admin classes for better user experience in the admin panel
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('value', 'unit', 'time', 'sensor')
+    list_filter = ('sensor', 'unit', 'time')
+
+
+class MeasureAdmin(admin.ModelAdmin):
+    list_display = ('unit', 'description')
+
+
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ('room', 'description')
+    list_filter = ('room', 'measurements')
+
+
+admin.site.register(Measure, MeasureAdmin)
+admin.site.register(Measurement, MeasurementAdmin)
+admin.site.register(Sensor, SensorAdmin)
---
+++ weathersite/sensordata/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Measure(models.Model):
+    """Used to specify which measurement can a sensor do (f. ex Temeprature)"""
+    unit = models.CharField(max_length=10)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.description
+
+
+class Sensor(models.Model):
+    """Model used to store information about sensors"""
+    # Fields
+    room = models.CharField(
+        max_length=20, help_text='Enter the room in which the sensor sits')
+    measurements = models.ManyToManyField(Measure)
+    description = models.CharField(
+        max_length=200, help_text="Enter, if necessary, additional information about the sensor")
+
+    def __str__(self):
+        """String representating the sensor"""
+        return f"Sensor {self.room}"
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular instance of the model"""
+        return reverse("model-detail-view", args=[str(self.id)])
+
+
+class Measurement(models.Model):
+    """Class used to store each individual measurement made by a sensor"""
+    value = models.FloatField()
+    unit = models.ForeignKey('Measure', on_delete=models.CASCADE)
+    sensor = models.ForeignKey('Sensor', on_delete=models.CASCADE)
+    time = models.TimeField(auto_now=False, auto_now_add=True)
+
+    class Meta:
+        models.UniqueConstraint(
+            fields=['time', 'sensor'], name="unique_measure_sensor")
+
+    def __str__(self):
+        return f"{self.value}{self.unit.unit} ({self.time})"
